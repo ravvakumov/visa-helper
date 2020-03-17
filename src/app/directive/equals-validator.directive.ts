645ebeb1f71ee9c347ec39bff6a779442f6b440e
avvakumov <avvakymov@mail.ru>
2020-03-17 17:15:07 +0300

    equals validator for password working

--- src/app/directive/equals-validator.directive.ts
+++ src/app/directive/equals-validator.directive.ts
@@ -1,25 +1,35 @@
 import {Directive, Input, NgModule, OnInit, ViewChild} from '@angular/core';
-import {AbstractControl, NG_VALIDATORS, NgControl, NgForm, ValidationErrors, Validator, Validators} from '@angular/forms';
+import {
+  AbstractControl,
+  FormGroup,
+  NG_VALIDATORS,
+  NgControl,
+  NgForm,
+  ValidationErrors,
+  Validator,
+  ValidatorFn,
+  Validators
+} from '@angular/forms';
+
+export const equalsValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  var validationErrors = password && confirmPassword && password.value === confirmPassword.value ? null : {'equalsValidator': true};
+
+  return validationErrors;
+};
 
 @Directive({
   selector: '[appEqualsValidator]',
   providers: [{provide: NG_VALIDATORS, useExisting: EqualsValidatorDirective, multi: true}]
 })
-// @NgModule({
-//   providers: [{provide: NG_VALIDATORS, useExisting: EqualsValidatorDirective, multi: true}]
-// })
 export class EqualsValidatorDirective implements Validator {
-  // @Input('appEqualsValidator') controlToCompare: AbstractControl; // What is the name of @Input('?'). Need to take password value.
-  // @Input('') signupForm: NgForm;
+  constructor() {
+  }
 
-  constructor(
-    // Get the control directive
-    // private originControl: NgControl
-  ) {}
-
-  validate(confirmationControl: AbstractControl): ValidationErrors | null {
-    console.log(confirmationControl.value); // Value of confirmPassword input
-    // console.log(this.originControl.value);
-    return null;
+  validate(control: AbstractControl): ValidationErrors {
+    console.log(control.value); // Value of confirmPassword input
+    return equalsValidator(control);
   }
 }
